2e56c734c0b14ff2ea314c9da3c4e648f4bf6427
Pratikshapprabhu <pratikshaprabhu211@gmail.com>
2021-12-16 20:22:58 +0530

    yum_recipe.py added

---
+++ yum_recipe.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+import os
+
+class Article:
+    def get_name(self, sp):
+        return sp.find('div',class_ = 'imo').span.text
+
+    def get_ing(self, sp):
+        div = sp.find('div',class_ = 'ingrdnt').find_all('p')[5:]
+        return list(map(lambda x:x.text,div))
+
+    def get_rec(self, sp):
+        div = sp.find('div',class_= 'prptn').find_all('p')
+        return list(map(lambda x:x.text,div))
+        
+    def get_des(self, sp):
+        return ''
+
+    def __init__(self, link):
+        content = requests.get(link)
+        sp = bs(content.text, 'lxml')
+        self.name = self.get_name(sp)
+        self.ing = self.get_ing(sp)
+        self.rec = self.get_rec(sp)
+        self.des = self.get_des(sp)
+
+
+if (__name__ == "__main__"):
+    content = requests.get('https://www.yumrecipe.in/')
+    sp = bs(content.text, 'lxml')
+    lnk_list = sp.find_all('div', class_='latest-feed')
+    links = []
+    #if file doesnot exist create the file
+    if not os.path.exists('project/webscraping/data/web_data.csv'):
+        df = pd.DataFrame(columns=['Name','Description','Ingredients','Recipe'])
+        df.to_csv('data/web_data.csv', index=False)
+        
+    for link in lnk_list:
+        lnk = link.find('ul').li.span.div.a["href"]
+        links.append(lnk)
+    for link in links:
+        art = Article(link)
+        print(link)
+        data = [art.name, art.des, "\n".join(art.ing), "\n".join(art.rec)]
+        df2 = pd.DataFrame([data])
+        df2.to_csv('data/web_data.csv', mode="a", index=False, header=False)
