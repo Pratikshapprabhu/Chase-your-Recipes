893585f54f340f1e1b644ba52fa174f4b6c2bfb4
PratikshaPrabhu <pratikshaprabhu211@gmail.com>
2022-02-08 17:02:19 +0000

    fixed yum_recipe not parsing

--- cookbook/webscraping/yum_recipe.py
+++ cookbook/webscraping/yum_recipe.py
@@ -18,7 +18,11 @@ def get_des(sp):
     return ''
 
 def get_img(link):
-    return link.find('img')['src']
+    try:
+        img_tag = link.find('img')['src']
+    except TypeError:
+        return ""
+    return img_tag
 
 def get_article(link):
     content = requests.get(link)
@@ -32,23 +36,17 @@ def get_article(link):
     domain = url_obj.hostname
     store_obj = RecipeStore(url=link,ingredients=ing,preparation=rec,desc=des)
     store_obj.save()
-    index = RecipeIndex(url=store_obj,recipe_name=name,img_url=img,domain=domain)    
+    index = RecipeIndex(recipe=store_obj,recipe_name=name,img_url=img,domain=domain)    
     index.save()
 
 def scrape_all():
     content = requests.get('https://www.yumrecipe.in/')
     sp = bs(content.text, 'lxml')
     lnk_list = sp.find_all('div', class_='latest-feed')
-    links = []
-    recipes = []
     for link in lnk_list:
         lnk = link.find('ul').li.span.div.a["href"]
-        links.append(lnk)
-    for link in links:
-        art = get_article(link)
-        print(link)
-        recipes.append(art)
-    return recipes
+        print(lnk)
+        get_article(lnk)
 
 if (__name__ == "__main__"):
-    art = scrape_all()
+    scrape_all()
